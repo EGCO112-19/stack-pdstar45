2c07c6181354023ccd3ecd1a427768a8b6ddd84c
PISIT LOUNSENG <pisit.lou@student.mahidol.edu>
2021-02-26 10:36:34 +0000

    }} core dumped

--- main.c
+++ main.c
@@ -6,24 +6,49 @@
 int main(int argc, char **argv){
   printf("Checking the parentheses in argv arguments\n");
   int i,N,j;
+  int C;
+  C =0;
   
   Stack s;
+  s.size=0;
+  s.top=NULL;
 
 
  
   for(i=1;i<argc;i++){
    
      for(j=0;j<strlen(argv[i]);j++){
+        char cut;
        /* Use stack to help with the parentheses*/
-
-
-     }
-
+       if(argv[i][j]== '[' || argv[i][j] == '{'){
+         push(&s,argv[i][j]);
+         s.size += 1;
+       }
+       if(argv[i][j]== ']'){
+         cut = pop(&s);
+         s.size-= 1;
+          if(cut != '[')
+           {C=1;
+            break;
+           }
+       }
+       if(argv[i][j]== '}'){
+         cut = pop(&s);
+         s.size -= 1;
+          if(cut!= '}')
+           {C=1;
+            break;
+           }
+       }
+    }
+     if(s.size!=0 ||C==1 ){
+       printf("the parentheses don't match successfull in %s \n",argv[i]);
+       pop_all(&s);
+       
+     }else
+       printf("the parentheses  match successfull in %s \n",argv[i]);
+   
 
   }
-
-
-
-
    return 0;
 }
--- stack.h
+++ stack.h
@@ -8,7 +8,36 @@ typedef struct {
 }Stack;
 
 typedef Stack * StackPtr;
-void push(StackPtr s, char value);
-char pop(StackPtr s);
-void pop_all(StackPtr s);
+void push(StackPtr s, char value){
+  Node *new_node=(Node *)malloc(sizeof(Node));
+  if(new_node){
+   new_node->data=value;
+   new_node-> nextPtr=s->top;
+   s->top=new_node;
+   }
+}
+
+char pop(StackPtr s){
+  NodePtr t = s->top;
+ char value;
+ t= s->top;
+ value=t->data;
+s->top=t->nextPtr;
+ free(t);
+ return value;
+}
+
+void pop_all(StackPtr s){
+  char value;
+  NodePtr t =s->top;
+  while(s->top !=NULL){
+    value=t->data;
+    s->top=t->nextPtr;
+    printf("popping %c \n",value);
+    free(t);
+    t=s->top;
+    s->size-=1;
+  }
+ 
+}
 #endif
